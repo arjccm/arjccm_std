5eee449e88ecbcad86f72f4ff51d57c51a2ca647
yi21 <yiqingxuan21@163.com>
2020-02-19 01:21:17 +0800

    补提交

---
+++ src/main/java/com/arjjs/ccm/modules/ccm/ccmsys/entity/CcmAreaDev.java
@@ -0,0 +1,40 @@
+package com.arjjs.ccm.modules.ccm.ccmsys.entity;
+
+public class CcmAreaDev {
+
+    private String type;
+    private String areaId;
+    private String areaName;
+    private String parentId;
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(String areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+}
