f771a1daab0ceb0770c64dfd8f15722f60278815
maoxb <42483363+pymxb1991@users.noreply.github.com>
2020-02-18 16:21:21 +0800

    【场所管理】医疗中心，文化教育，增加地图标注

--- src/main/java/com/arjjs/ccm/modules/ccm/hospital/entity/CcmPlaceHospital.java
+++ src/main/java/com/arjjs/ccm/modules/ccm/hospital/entity/CcmPlaceHospital.java
@@ -50,6 +50,25 @@ public class CcmPlaceHospital extends DataEntity<CcmPlaceHospital> {
 
 	private Area area;
 	private User checkUser;
+	private String areaMap;		// 区域图
+	private String areaPoint;		// 中心点
+
+	public String getAreaMap() {
+		return areaMap;
+	}
+
+	public void setAreaMap(String areaMap) {
+		this.areaMap = areaMap;
+	}
+
+	public String getAreaPoint() {
+		return areaPoint;
+	}
+
+	public void setAreaPoint(String areaPoint) {
+		this.areaPoint = areaPoint;
+	}
+
 	public List<String> getKeyPointList() {
 		List<String> list = Lists.newArrayList();
 		if (ccmBasePlace!=null&&ccmBasePlace.getKeyPoint() != null) {
