64e70e8614b90b2b57d96b74ea747db015a5625a
shenzhilei <471798272@qq.com>
2020-02-19 22:15:27 +0800

    【新增功能】宗教活动管理重点人员模块

---
+++ src/main/java/com/arjjs/ccm/modules/sys/web/KeyPersonalController.java
@@ -0,0 +1,50 @@
+package com.arjjs.ccm.modules.sys.web;
+
+
+import com.arjjs.ccm.modules.sys.entity.KeyPersonal;
+import com.arjjs.ccm.modules.sys.service.KeyPersonalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "${adminPath}/sys/map/KeyPersonal")
+public class KeyPersonalController {
+
+    @Autowired
+    private KeyPersonalService keyPersonalService;
+
+    //查询最近时间段
+    @RequestMapping(value = "/recently",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> findPersonal(Date time){
+        Map<String,Object> map = new HashMap();
+        List<KeyPersonal> Plist = keyPersonalService.findPersonal(time);
+
+        map.put("code","200");
+        map.put("data",Plist);
+        return map;
+    }
+
+
+
+    //开始时间结束时间
+    @RequestMapping(value = "/KJ",method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,Object> findPersonalAday(String time1,String time2){
+        Map<String,Object> map = new HashMap();
+        List<KeyPersonal> Plist = keyPersonalService.findPersonalKJ(time1,time2);
+
+        map.put("code","200");
+        map.put("data",Plist);
+        return map;
+    }
+}
