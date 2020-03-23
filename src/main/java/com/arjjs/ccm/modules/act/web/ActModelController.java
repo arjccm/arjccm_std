a027c387f72b6550c1f6b3a24ce059d648f8836d
genghaoshuai <1814976578@qq.com>
2020-03-23 16:14:49 +0800

    【后台】 修改流程管理中空模型部署报错问题

--- src/main/java/com/arjjs/ccm/modules/act/web/ActModelController.java
+++ src/main/java/com/arjjs/ccm/modules/act/web/ActModelController.java
@@ -3,9 +3,12 @@
  */
 package com.arjjs.ccm.modules.act.web;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.arjjs.ccm.tool.CommUtil;
+import org.activiti.engine.ActivitiException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,8 +21,12 @@ import com.arjjs.ccm.common.persistence.Page;
 import com.arjjs.ccm.common.web.BaseController;
 import com.arjjs.ccm.modules.act.service.ActModelService;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * 流程模型相关Controller
+ *
  * @author admin001
  * @version 2013-11-03
  */
@@ -27,93 +34,112 @@ import com.arjjs.ccm.modules.act.service.ActModelService;
 @RequestMapping(value = "${adminPath}/act/model")
 public class ActModelController extends BaseController {
 
-	@Autowired
-	private ActModelService actModelService;
+    @Autowired
+    private ActModelService actModelService;
 
-	/**
-	 * 流程模型列表
-	 */
-	@RequiresPermissions("act:model:edit")
-	@RequestMapping(value = { "list", "" })
-	public String modelList(String category, HttpServletRequest request, HttpServletResponse response, Model model) {
+    /**
+     * 流程模型列表
+     */
+    @RequiresPermissions("act:model:edit")
+    @RequestMapping(value = {"list", ""})
+    public String modelList(String category, HttpServletRequest request, HttpServletResponse response, Model model) {
 
-		Page<org.activiti.engine.repository.Model> page = actModelService.modelList(
-				new Page<org.activiti.engine.repository.Model>(request, response), category);
+        Page<org.activiti.engine.repository.Model> page = actModelService.modelList(
+                new Page<org.activiti.engine.repository.Model>(request, response), category);
 
-		model.addAttribute("page", page);
-		model.addAttribute("category", category);
+        model.addAttribute("page", page);
+        model.addAttribute("category", category);
 
 
-		return "modules/act/actModelList";
-	}
+        return "modules/act/actModelList";
+    }
 
-	/**
-	 * 创建模型
-	 */
-	@RequiresPermissions("act:model:edit")
-	@RequestMapping(value = "create", method = RequestMethod.GET)
-	public String create(Model model) {
-		return "modules/act/actModelCreate";
-	}
-	
-	/**
-	 * 创建模型
-	 */
-	@RequiresPermissions("act:model:edit")
-	@RequestMapping(value = "create", method = RequestMethod.POST)
-	public void create(String name, String key, String description, String category,
-			HttpServletRequest request, HttpServletResponse response) {
+    /**
+     * 创建模型
+     */
+    @RequiresPermissions("act:model:edit")
+    @RequestMapping(value = "create", method = RequestMethod.GET)
+    public String create(Model model) {
+        return "modules/act/actModelCreate";
+    }
+
+    /**
+     * 创建模型
+     */
+    @RequiresPermissions("act:model:edit")
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public void create(String name, String key, String description, String category,
+                       HttpServletRequest request, HttpServletResponse response) {
+        try {
+            org.activiti.engine.repository.Model modelData = actModelService.create(name, key, description, category);
+            response.sendRedirect(request.getContextPath() + "/act/process-editor/modeler.jsp?modelId=" + modelData.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("创建模型失败：", e);
+        }
+    }
+
+    /**
+     * 根据Model部署流程
+     */
+    @RequiresPermissions("act:model:edit")
+    @RequestMapping(value = "deploy")
+    public String deploy(String id, RedirectAttributes redirectAttributes) {
+		String message = null;
 		try {
-			org.activiti.engine.repository.Model modelData = actModelService.create(name, key, description, category);
-			response.sendRedirect(request.getContextPath() + "/act/process-editor/modeler.jsp?modelId=" + modelData.getId());
+			message = actModelService.deploy(id);
 		} catch (Exception e) {
 			e.printStackTrace();
-			logger.error("创建模型失败：", e);
+            redirectAttributes.addFlashAttribute("message", "部署模型不能为空，模块ID=" + id);
+            return "redirect:" + adminPath + "/act/model";
 		}
-	}
-
-	/**
-	 * 根据Model部署流程
-	 */
-	@RequiresPermissions("act:model:edit")
-	@RequestMapping(value = "deploy")
-	public String deploy(String id, RedirectAttributes redirectAttributes) {
-		String message = actModelService.deploy(id);
 		redirectAttributes.addFlashAttribute("message", message);
 		return "redirect:" + adminPath + "/act/process";
 	}
-	
+
 	/**
 	 * 导出model的xml文件
 	 */
 	@RequiresPermissions("act:model:edit")
 	@RequestMapping(value = "export")
 	public void export(String id, HttpServletResponse response) {
-		actModelService.export(id, response);
-	}
+        PrintWriter out = null;
+        try {
+            actModelService.export(id, response);
+        } catch (ActivitiException e) {
+            e.printStackTrace();
+            try {
+                out = response.getWriter();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            CommUtil.openWinExpDiv(out, "导出模型不能为空，模块ID=" + id);
+        }
+    }
 
-	/**
-	 * 更新Model分类
-	 */
-	@RequiresPermissions("act:model:edit")
-	@RequestMapping(value = "updateCategory")
-	public String updateCategory(String id, String category, RedirectAttributes redirectAttributes) {
-		actModelService.updateCategory(id, category);
-		redirectAttributes.addFlashAttribute("message", "设置成功，模块ID=" + id);
-		return "redirect:" + adminPath + "/act/model";
-	}
-	
-	/**
-	 * 删除Model
-	 * @param id
-	 * @param redirectAttributes
-	 * @return
-	 */
-	@RequiresPermissions("act:model:edit")
-	@RequestMapping(value = "delete")
-	public String delete(String id, RedirectAttributes redirectAttributes) {
-		actModelService.delete(id);
-		redirectAttributes.addFlashAttribute("message", "删除成功，模型ID=" + id);
-		return "redirect:" + adminPath + "/act/model";
-	}
+    /**
+     * 更新Model分类
+     */
+    @RequiresPermissions("act:model:edit")
+    @RequestMapping(value = "updateCategory")
+    public String updateCategory(String id, String category, RedirectAttributes redirectAttributes) {
+        actModelService.updateCategory(id, category);
+        redirectAttributes.addFlashAttribute("message", "设置成功，模块ID=" + id);
+        return "redirect:" + adminPath + "/act/model";
+    }
+
+    /**
+     * 删除Model
+     *
+     * @param id
+     * @param redirectAttributes
+     * @return
+     */
+    @RequiresPermissions("act:model:edit")
+    @RequestMapping(value = "delete")
+    public String delete(String id, RedirectAttributes redirectAttributes) {
+        actModelService.delete(id);
+        redirectAttributes.addFlashAttribute("message", "删除成功，模型ID=" + id);
+        return "redirect:" + adminPath + "/act/model";
+    }
 }
