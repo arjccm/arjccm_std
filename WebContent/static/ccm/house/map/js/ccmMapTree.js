dd69d82417182ab727f8f6be671497796ef76d7d
yi21 <yiqingxuan21@163.com>
2020-03-26 14:59:03 +0800

    【bug修改】地理信息服务平台 地图标注显示

--- WebContent/static/ccm/house/map/js/ccmMapTree.js
+++ WebContent/static/ccm/house/map/js/ccmMapTree.js
@@ -73,9 +73,9 @@ $(function() {
 		var areaMap=selectedNodes[0].areaMap;
 		var id=selectedNodes[0].id;
 		var treeTypeData=selectedNodes[0].type;
-
+		var name=selectedNodes[0].name;
 		if (selectedNodes[0].checked) {//该节点未选中时，点击完变为选中状态
-			// Map.markInfo(id,treeTypeData,selectedNodes,name);
+			Map.markInfo(id,treeTypeData,selectedNodes,name);
 			if(areaPoint==""&&areaMap==""){
 				// top.$.jBox.tip('请标注添加坐标信息');
 //				Map.removeLayer('vectorMark'); //pengjianqiang
