1d45465e374c929f21025666c1ce729f4d0dc651
shenzhilei <471798272@qq.com>
2020-04-30 17:55:02 +0800

    【新功能开发】app地图场所（新设计）

--- src/main/java/com/arjjs/ccm/modules/ccm/rest/web/CcmRestKeyPlace.java
+++ src/main/java/com/arjjs/ccm/modules/ccm/rest/web/CcmRestKeyPlace.java
@@ -1,21 +1,32 @@
 package com.arjjs.ccm.modules.ccm.rest.web;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.arjjs.ccm.common.config.Global;
 import com.arjjs.ccm.common.utils.StringUtils;
+import com.arjjs.ccm.modules.ccm.org.entity.SysArea;
+import com.arjjs.ccm.modules.ccm.org.service.SysAreaService;
 import com.arjjs.ccm.modules.ccm.place.base.entity.CcmBasePlace;
 import com.arjjs.ccm.modules.ccm.place.base.service.CcmBasePlaceService;
+import com.arjjs.ccm.modules.sys.entity.Area;
 import com.arjjs.ccm.tool.CommUtilRest;
+import com.arjjs.ccm.tool.geoJson.Features;
+import com.arjjs.ccm.tool.geoJson.GeoJSON;
+import com.arjjs.ccm.tool.geoJson.Geometry;
+import com.arjjs.ccm.tool.geoJson.Properties;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.arjjs.ccm.common.persistence.Page;
@@ -36,6 +47,8 @@ import com.arjjs.ccm.modules.sys.entity.User;
 @RequestMapping(value = "${appPath}/rest/keyPlace")
 public class CcmRestKeyPlace extends BaseController {
 
+    @Autowired
+    private SysAreaService sysAreaService;
     @Autowired
     private CcmOrgNpseService ccmOrgNpseService;
     @Autowired
@@ -90,24 +103,147 @@ public class CcmRestKeyPlace extends BaseController {
             return result;
         }
         ccmBasePlace.setCheckUser(sessionUser);
-        Page<CcmBasePlace> page = ccmBasePlaceService.findPage(new Page<>(req, resp), ccmBasePlace);
+            Page<CcmBasePlace> page = ccmBasePlaceService.findPage(new Page<>(req, resp), ccmBasePlace);
 
-        List<CcmBasePlace> list = page.getList();
-        List<CcmBasePlace> resultList = Lists.newArrayList();
-        list.forEach(ccmBasePlace1->{
-            String file1 = ccmBasePlace1.getPlacePicture();
-            if (StringUtils.isNotEmpty(file1)) {
-                String fileUrl = Global.getConfig("FILE_UPLOAD_URL");
-                ccmBasePlace1.setPlacePicture(fileUrl + file1);
+            List<CcmBasePlace> list = page.getList();
+            List<CcmBasePlace> resultList = Lists.newArrayList();
+            list.forEach(ccmBasePlace1->{
+                String file1 = ccmBasePlace1.getPlacePicture();
+                if (StringUtils.isNotEmpty(file1)) {
+                    String fileUrl = Global.getConfig("FILE_UPLOAD_URL");
+                    ccmBasePlace1.setPlacePicture(fileUrl + file1);
+                }
+                resultList.add(ccmBasePlace1);
+            });
+
+            result.setCode(CcmRestType.OK);
+            result.setResult(page.getList());
+            return result;
+    }
+
+
+    /**
+     * @see 生成场所地图信息-点位图（分页模式）
+     * @param ccmBasePlace
+     * @param model
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "queryPlaceMap")
+    public CcmRestResult queryPlaceMap(String userId,CcmBasePlace ccmBasePlace, HttpServletRequest request, HttpServletResponse response,
+                                 @RequestParam(required = false) int pageNo, @RequestParam(required = false) int pageSize) {
+
+        CcmRestResult result = new CcmRestResult();
+        User sessionUser = (User) request.getSession().getAttribute("user");
+        if (sessionUser == null) {
+            result.setCode(CcmRestType.ERROR_USER_NOT_EXIST);
+            return result;
+        }
+        String sessionUserId = sessionUser.getId();
+        if (userId == null || "".equals(userId) || !userId.equals(sessionUserId)) {
+            result.setCode(CcmRestType.ERROR_USER_NOT_EXIST);
+            return result;
+        }
+        ccmBasePlace.setCheckUser(sessionUser);
+
+        //分页参数处理
+        Page pageIn = new Page<CcmBasePlace>(request, response);
+        if (pageNo != 0) {
+            pageIn.setPageNo(pageNo);
+        }
+        if (pageSize != 0) {
+            pageIn.setPageSize(pageSize);
+        }
+
+        // 查询地图场所信息
+        List<CcmBasePlace> ccmBasePlacelist = new ArrayList<CcmBasePlace>();
+        //可以选择父节点查询
+        if(ccmBasePlace.getArea()!=null&&ccmBasePlace.getArea().getId()!=null&&!ccmBasePlace.getArea().getId().equals("")){
+            SysArea sysArea = sysAreaService.get(ccmBasePlace.getArea().getId());
+            Area area = new Area();
+            area.setId(sysArea.getId());
+            area.setParentIds(sysArea.getParentIds());
+            ccmBasePlace.setUserArea(area);
+            ccmBasePlace.setArea(null);
+        }
+        Page<CcmBasePlace> page = ccmBasePlaceService.findPage(pageIn, ccmBasePlace);
+        ccmBasePlacelist = page.getList();
+
+        // 返回对象
+        GeoJSON geoJSON = new GeoJSON();
+
+        geoJSON.setCount(page.getCount());
+        geoJSON.setPageNo(page.getPageNo());
+        geoJSON.setPageSize(page.getPageSize());
+
+        List<Features> featureList = new ArrayList<Features>();
+        // 数组
+        for (CcmBasePlace basePlace : ccmBasePlacelist) {
+            // 特征,属性
+            Features featureDto = new Features();
+            Properties properties = new Properties();
+            // 1 type 默认不填
+            // 2 id 添加
+            featureDto.setId(basePlace.getId());
+            // 3 properties 展示属性信息
+            properties.setName(basePlace.getPlaceName());
+            properties.setIcon(basePlace.getPlacePicture());
+            // 点击后展示详细属性值
+            Map<String, Object> map_P = new HashMap<String, Object>();
+            // 创建附属信息
+            map_P.put("场所名称", basePlace.getPlaceName());
+            map_P.put("重点属性", basePlace.getKeyPoint());
+            map_P.put("场所面积", basePlace.getPlaceArea());
+            map_P.put("场所用途", basePlace.getPlaceUse());
+            map_P.put("负责人姓名", basePlace.getLeaderName());
+            map_P.put("负责人电话", basePlace.getLeaderContact());
+            map_P.put("主管单位名称", basePlace.getGoverningBodyName());
+            map_P.put("地址", basePlace.getAddress());
+            map_P.put("场所类型", basePlace.getPlaceType());
+            map_P.put("场所图片", basePlace.getPlacePicture());
+            properties.addInfo(map_P);
+            featureList.add(featureDto);
+            featureDto.setProperties(properties);
+            // 4 geometry 配置参数
+            Geometry geometry = new Geometry();
+            featureDto.setGeometry(geometry);
+            // 点位信息 测试为点
+            geometry.setType("Point");
+            // 为中心点赋值
+            if (!StringUtils.isEmpty(basePlace.getAreaPoint())) {
+                // 获取中心点的值
+                String[] centpoint = basePlace.getAreaPoint().split(",");
+                geoJSON.setCentpoint(centpoint);
+                // 添加图形中心点位信息
+                properties.setCoordinateCentre(centpoint);
             }
-            resultList.add(ccmBasePlace1);
-        });
 
+            List<String> Coordinateslist = new ArrayList<>();
+            // 当前是否为空如果为空则进行添加空数组 ，否则进行拆分添加数据
+            String[] a = (StringUtils.isEmpty(basePlace.getAreaPoint()) ? (",") : basePlace.getAreaPoint()).split(",");
+            // 填充数据
+            if (a.length < 2) {
+                Coordinateslist.add("");
+                Coordinateslist.add("");
+            } else {
+                Coordinateslist.add(a[0]);
+                Coordinateslist.add(a[1]);
+            }
+            // 装配点位
+            geometry.setCoordinates(Coordinateslist);
+
+        }
+        // 添加数据
+        geoJSON.setFeatures(featureList);
+        // 如果无数据
+        if (featureList.size() == 0) {
+            return null;
+        }
         result.setCode(CcmRestType.OK);
-        result.setResult(page.getList());
-
+        result.setResult(geoJSON);
         return result;
     }
+
 
     /**
      * 修改重点场所信息
