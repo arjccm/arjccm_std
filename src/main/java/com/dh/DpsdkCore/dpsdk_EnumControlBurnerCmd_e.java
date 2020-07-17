3560310c21bf49090211bdcf422d6e9524e551fb
hu <hwjlmz@sina.com>
2020-07-17 10:04:02 +0800

    大华设备对接

---
+++ src/main/java/com/dh/DpsdkCore/dpsdk_EnumControlBurnerCmd_e.java
@@ -0,0 +1,18 @@
+package com.dh.DpsdkCore;
+
+/** ¿ÌÂ¼¿ØÖÆÃüÁî
+@param    Cmd_StartBurn		        = 1;					¿ªÊ¼¿ÌÂ¼
+@param    Cmd_PauseBurn		        = 2;					ÔİÍ£¿ÌÂ¼
+@param    Cmd_StartBurn		        = 3;					¼ÌĞø¿ÌÂ¼
+@param    Cmd_PauseBurn		        = 4;					Í£Ö¹¿ÌÂ¼
+@param    Cmd_StartBurn		        = 5;					ÊÖ¶¯»»ÅÌ
+*/
+
+public class dpsdk_EnumControlBurnerCmd_e
+{
+	public static final int	Cmd_StartBurn 		= 1;		// ¿ªÊ¼¿ÌÂ¼
+	public static final int Cmd_PauseBurn		= 2;		// ÔİÍ£¿ÌÂ¼
+	public static final int	Cmd_ContinueBurn	= 3;		// ¼ÌĞø¿ÌÂ¼
+	public static final int	Cmd_StopBurn		= 4;		// Í£Ö¹¿ÌÂ¼
+	public static final int	Cmd_ChangeCD		= 5;		// ÊÖ¶¯»»ÅÌ
+};
